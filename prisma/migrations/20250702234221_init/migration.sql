caa6fe9f8658bf8960b87bdf656a3d7d268f3184
Utech17 <127623759+Utech17@users.noreply.github.com>
2025-07-02 22:55:45 -0400

    migracion pa postgre

--- prisma/migrations/20250702234221_init/migration.sql
+++ prisma/migrations/20250702234221_init/migration.sql
@@ -1,22 +1,16 @@
 -- CreateTable
-CREATE TABLE `Model` (
-    `modelCode` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`modelCode`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE TABLE "Model" (
+    "modelCode" VARCHAR(191) PRIMARY KEY,
+    "description" VARCHAR(191) NOT NULL
+);
 
 -- CreateTable
-CREATE TABLE `Unit` (
-    `plate` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `modelCode` VARCHAR(191) NOT NULL,
-    `seatCount` INTEGER NOT NULL,
-    `type` VARCHAR(191) NOT NULL,
-    `year` INTEGER NOT NULL,
-
-    PRIMARY KEY (`plate`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Unit` ADD CONSTRAINT `Unit_modelCode_fkey` FOREIGN KEY (`modelCode`) REFERENCES `Model`(`modelCode`) ON DELETE RESTRICT ON UPDATE CASCADE;
+CREATE TABLE "Unit" (
+    "plate" VARCHAR(191) PRIMARY KEY,
+    "description" VARCHAR(191) NOT NULL,
+    "modelCode" VARCHAR(191) NOT NULL,
+    "seatCount" INTEGER NOT NULL,
+    "type" VARCHAR(191) NOT NULL,
+    "year" INTEGER NOT NULL,
+    CONSTRAINT "Unit_modelCode_fkey" FOREIGN KEY ("modelCode") REFERENCES "Model"("modelCode") ON DELETE RESTRICT ON UPDATE CASCADE
+);
